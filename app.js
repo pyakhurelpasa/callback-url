a111b078c6ff44f417e1cff2df5fc6d87decdf33
Madhukar Gaida <shroudmg@gmail.com>
2023-09-27 14:24:24 +0545

    add immport ethers

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 const port = 3000;
 
-// mongoDB details
+const ethers = require("ethers");
 require("dotenv").config();
 const bodyParser = require("body-parser");
 const contractABI = require("./CIDVerifier.json");
