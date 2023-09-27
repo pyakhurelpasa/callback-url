a41204abadb68ba265aa940b3170da63a73c6154
Madhukar Gaida <shroudmg@gmail.com>
2023-09-27 11:34:15 +0545

    uri to cid bug fix

--- src/helpers/uriToCID.js
+++ src/helpers/uriToCID.js
@@ -1,6 +1,8 @@
 // Extracts CID from specific URI format
 
-export function uriToCID(url) {
+function uriToCID(url) {
   const matches = url.match(/\/([a-z0-9]+)\./i);
   return matches ? matches[1] : null;
 }
+
+module.exports = uriToCID;
